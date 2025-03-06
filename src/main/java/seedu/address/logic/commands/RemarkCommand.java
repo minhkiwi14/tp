243d11efc73fb7fc5d00f9c9cc2f1052c9e687b0
minhkiwi14 <minhnn1472005@gmail.com>
2025-03-07 01:42:35 +0800

    Add remark tests

--- src/main/java/seedu/address/logic/commands/RemarkCommand.java
+++ src/main/java/seedu/address/logic/commands/RemarkCommand.java
@@ -9,6 +9,8 @@ public class RemarkCommand extends Command {
 
     public static final String COMMAND_WORD = "remark";
 
+    public static final String MESSAGE_NOT_IMPLEMENTED_YET = "Remark command not implemented yet";
+
     @Override
     public CommandResult execute(Model model) {
         return new CommandResult("Hello from remark");
