f7589d1ea787a20d573e241804c2d8a789a2b753
minhkiwi14 <minhnn1472005@gmail.com>
2025-03-07 12:54:58 +0800

    Update RemarkCommand and RemarkCommandParser

--- src/test/java/seedu/address/logic/parser/RemarkCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/RemarkCommandParserTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.RemarkCommand;
+import seedu.address.model.person.Remark;
 
 public class RemarkCommandParserTest {
     private RemarkCommandParser parser = new RemarkCommandParser();
@@ -25,7 +26,7 @@ public class RemarkCommandParserTest {
 
         // no remark
         userInput = targetIndex.getOneBased() + " " + PREFIX_REMARK;
-        expectedCommand = new RemarkCommand(INDEX_FIRST_PERSON, "");
+        expectedCommand = new RemarkCommand(INDEX_FIRST_PERSON, new Remark(""));
         assertParseSuccess(parser, userInput, expectedCommand);
     }
 
