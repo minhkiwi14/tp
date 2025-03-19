76b2d0aee50c510d1197d5a97c22d22957787d19
0xF41 <faiyew@DESKTOP-ARR737S.>
2025-03-19 20:10:12 +0800

    Fix AddressBookParserTest#parseCommand_add

--- src/test/java/seedu/address/testutil/PersonUtil.java
+++ src/test/java/seedu/address/testutil/PersonUtil.java
@@ -1,24 +1,16 @@
 package seedu.address.testutil;
 
-import static seedu.address.logic.parser.CliSyntax.PREFIX_ATTENDANCE;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_COURSE;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_GRADE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_ID;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_NOTE;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_PARTICIPATION;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
 
 import seedu.address.logic.commands.AddCommand;
-import seedu.address.logic.commands.EditCommand.EditPersonDescriptor;
 import seedu.address.model.person.Person;
 
 /**
  * A utility class for Person.
  */
 public class PersonUtil {
-// Used for testing
+    // Used for testing
     /**
      * Returns an add command string for adding the {@code person}.
      */
@@ -30,30 +22,11 @@ public class PersonUtil {
      * Returns the part of command string for the given {@code person}'s details.
      */
     public static String getPersonDetails(Person person) {
-        StringBuilder sb = new StringBuilder();
-        sb.append(PREFIX_NAME + person.getName().fullName + " ");
-        sb.append(PREFIX_PHONE + person.getPhone().value + " ");
-        sb.append(PREFIX_EMAIL + person.getEmail().value + " ");
-        return sb.toString();
+        return String.format("%s %s %s %s",
+                PREFIX_NAME,
+                person.getName().fullName,
+                PREFIX_ID,
+                person.getId().id);
     }
 
-    /**
-     * Returns the part of command string for the given {@code EditPersonDescriptor}'s details.
-     */
-    public static String getEditPersonDescriptorDetails(EditPersonDescriptor descriptor) {
-        StringBuilder sb = new StringBuilder();
-        descriptor.getNewId().ifPresent(id -> sb.append(PREFIX_ID).append(id.id).append(" "));
-        descriptor.getName().ifPresent(name -> sb.append(PREFIX_NAME).append(name.fullName).append(" "));
-        descriptor.getPhone().ifPresent(phone -> sb.append(PREFIX_PHONE).append(phone.value).append(" "));
-        descriptor.getEmail().ifPresent(email -> sb.append(PREFIX_EMAIL).append(email.value).append(" "));
-        descriptor.getCourse().ifPresent(course ->
-                sb.append(PREFIX_COURSE).append(course.course).append(" "));
-        descriptor.getAttendance().ifPresent(attendance ->
-                sb.append(PREFIX_ATTENDANCE).append(attendance.status).append(" "));
-        descriptor.getParticipation().ifPresent(participation ->
-                sb.append(PREFIX_PARTICIPATION).append(participation.status).append(" "));
-        descriptor.getGrade().ifPresent(grade ->sb.append(PREFIX_GRADE).append(grade.grade).append(" "));
-        descriptor.getNote().ifPresent(note -> sb.append(PREFIX_NOTE).append(note.message).append(" "));
-        return sb.toString();
-    }
 }
