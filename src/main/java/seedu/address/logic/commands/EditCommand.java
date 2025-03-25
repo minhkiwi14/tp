2158e1ca94ff9211463730bc42c46ecd8749d8df
Billy026 <92001719+Billy026@users.noreply.github.com>
2025-03-25 14:52:57 +0800

    Change output message of Edit command
    The output message uses a colon to separate both the command word
    and the updated properties.
    
    This may not be such an intuitive output message.
    
    Let's change the separator between the command word and the rest
    of the message to "-".
    
    This way, users can differentiate between the command word and
    updated fields.

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -59,7 +59,7 @@ public class EditCommand extends Command {
     public static final String MESSAGE_USAGE = String.format(MESSAGE_USAGE_UNFORMATTED, COMMAND_WORD,
             PREFIX_ID, PREFIX_NEW_ID, PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_COURSE);
 
-    public static final String MESSAGE_EDIT_PERSON_SUCCESS = "Edited Person: %1$s";
+    public static final String MESSAGE_EDIT_PERSON_SUCCESS = "Edited Person - %1$s";
     public static final String MESSAGE_NOT_EDITED = "At least one field to edit must be provided.";
     public static final String MESSAGE_DUPLICATE_PERSON = "This person already exists in the address book.";
 
