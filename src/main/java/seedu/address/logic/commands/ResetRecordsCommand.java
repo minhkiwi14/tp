71561cb6940748e92d66e87e1ced1511e13b5b15
minhkiwi14 <minhnn1472005@gmail.com>
2025-03-31 20:32:33 +0800

    Add skeleton for the class

---
+++ src/main/java/seedu/address/logic/commands/ResetRecordsCommand.java
@@ -0,0 +1,22 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+
+import seedu.address.model.Model;
+
+public class ResetRecordsCommand extends Command {
+    public static final String COMMAND_WORD = "resetRecords";
+
+    public static final String MESSAGE_SUCCESS = "All records have been reset.";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Resets all records in the address book.\n"
+            + "Example: " + COMMAND_WORD;
+
+    @Override
+    public CommandResult execute(Model model) {
+        requireNonNull(model);
+        // model.resetRecords();
+        return new CommandResult(MESSAGE_SUCCESS);
+    }
+}
