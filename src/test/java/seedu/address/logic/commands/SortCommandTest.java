b892a1e33be26317fff24611348865e7c18e6beb
ZaydM18 <zayd.m18@gmail.com>
2025-04-01 16:41:31 +0800

    Edit the Sort command to take in four different parameters (one at a time)
    Modify the SortParser class in accordance with the changes to the Sort
    command class.
    
    Create the helper SortDescriptor class and corresponding test class.

--- src/test/java/seedu/address/logic/commands/SortCommandTest.java
+++ src/test/java/seedu/address/logic/commands/SortCommandTest.java
@@ -1,39 +1,73 @@
 package seedu.address.logic.commands;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+import static seedu.address.testutil.TypicalPersons.*;
 
-import java.util.Comparator;
+import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
+import seedu.address.model.person.Person;
 
 public class SortCommandTest {
 
-    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
-    private Model expectedModel = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+    private Model model;
+
+    @BeforeEach
+    public void setUp() {
+        model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+    }
 
     @Test
-    public void execute_sortByName_success() {
-        expectedModel.sortFilteredPersonList(Comparator.comparing(p -> p.getName().toString()));
-        SortCommand sortCommand = new SortCommand();
+    public void execute_sortByName_correctOrder() throws Exception {
+        SortCommand.SortDescriptor descriptor = new SortCommand.SortDescriptor();
+        descriptor.setSortByName();
+        SortCommand command = new SortCommand(descriptor);
+        command.execute(model);
 
-        assertCommandSuccess(sortCommand, model, SortCommand.MESSAGE_SUCCESS, expectedModel);
+        List<Person> actual = model.getFilteredPersonList();
+        List<Person> expected = Arrays.asList(ALICE, BENSON, CARL, DANIEL, ELLE, FIONA, GEORGE);
+        assertEquals(expected, actual);
+    }
 
-        List<String> sortedNames = model.getFilteredPersonList().stream()
-                .map(p -> p.getName().toString())
-                .collect(Collectors.toList());
+    @Test
+    public void execute_sortByGrade_correctOrder() throws Exception {
+        SortCommand.SortDescriptor descriptor = new SortCommand.SortDescriptor();
+        descriptor.setSortByGrade();
+        SortCommand command = new SortCommand(descriptor);
+        command.execute(model);
 
-        List<String> expectedNames = expectedModel.getFilteredPersonList().stream()
-                .map(p -> p.getName().toString())
-                .collect(Collectors.toList());
+        List<Person> actual = model.getFilteredPersonList();
+        List<Person> expected = Arrays.asList(ALICE, ELLE, FIONA, DANIEL, BENSON, CARL, GEORGE);
+        assertEquals(expected, actual);
+    }
 
-        assertEquals(expectedNames, sortedNames);
+    @Test
+    public void execute_sortByAttendance_correctOrder() throws Exception {
+        SortCommand.SortDescriptor descriptor = new SortCommand.SortDescriptor();
+        descriptor.setSortByAttendance();
+        SortCommand command = new SortCommand(descriptor);
+        command.execute(model);
+
+        List<Person> actual = model.getFilteredPersonList();
+        List<Person> expected = Arrays.asList(ALICE, BENSON, CARL, DANIEL, ELLE, FIONA, GEORGE);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void execute_sortByParticipation_correctOrder() throws Exception {
+        SortCommand.SortDescriptor descriptor = new SortCommand.SortDescriptor();
+        descriptor.setSortByParticipation();
+        SortCommand command = new SortCommand(descriptor);
+        command.execute(model);
+
+        List<Person> actual = model.getFilteredPersonList();
+        List<Person> expected = Arrays.asList(ALICE, DANIEL, ELLE, BENSON, FIONA, CARL, GEORGE);
+        assertEquals(expected, actual);
     }
 }
