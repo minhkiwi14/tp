66af3541f91fd03e9e55c11a82ae86712f5e9e05
0xF41 <faiyew@DESKTOP-ARR737S.>
2025-04-02 23:29:36 +0800

    Fix AddCommandParser to reject invalid prefixes

--- src/main/java/seedu/address/logic/parser/AddCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddCommandParser.java
@@ -1,12 +1,7 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_COURSE;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_ID;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_NOTE;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
+import static seedu.address.logic.parser.CliSyntax.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -39,8 +34,23 @@ public class AddCommandParser implements Parser<AddCommand> {
      * @throws ParseException if the user input does not conform the expected format
      */
     public AddCommand parse(String args) throws ParseException {
-        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL,
-                PREFIX_ID, PREFIX_COURSE, PREFIX_NOTE);
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(
+                args,
+                PREFIX_NAME,
+                PREFIX_PHONE,
+                PREFIX_EMAIL,
+                PREFIX_ID,
+                PREFIX_COURSE,
+                PREFIX_NOTE);
+
+        // Check if any invalid prefixes are present
+        if (args.contains(PREFIX_GRADE.getPrefix())
+                || args.contains(PREFIX_ATTENDANCE.getPrefix())
+                || args.contains(PREFIX_PARTICIPATION.getPrefix())) {
+            throw new ParseException(
+                    "Grade, Attendance and Participation cannot be added with the add command. "
+                    + "Please add them using the edit command.");
+        }
 
         if (!arePrefixesPresent(argMultimap, PREFIX_NAME, PREFIX_ID)
                 || !argMultimap.getPreamble().isEmpty()) {
