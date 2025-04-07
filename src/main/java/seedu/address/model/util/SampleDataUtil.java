1d0febd02765b1e7909f541822d34645f6703157
minhkiwi14 <minhnn1472005@gmail.com>
2025-04-08 01:47:32 +0800

    Add remaining sample data

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -27,7 +27,16 @@ public class SampleDataUtil {
             List.of(new Note("Requested for a meeting"), new Note("Will be absent next week"))),
             new Person(new Id("A0123457N"), new Name("Jesse Pinkman"), new Phone("87654321"),
                 new Email("jesse.p@example.com"), new Course("HS2915"), new Attendance("PRESENT"),
-                new Participation("GOOD"), new Grade(96), List.of())
+                new Participation("GOOD"), new Grade(96), List.of()),
+            new Person(new Id("A0123458N"), new Name("Gus Fring"), new Phone("+65-1111-1111"),
+                new Email("gus.fring@example.com"), new Course("HS2915"), new Attendance("EXCUSED"),
+                new Participation("UNMARKED"), new Grade(100), List.of(new Note("los pollos hermanos"))),
+            new Person(new Id("HT0111111X"), new Name("Saul Goodman"), new Phone("12344321"),
+                new Email("saul_goodman@gmail.com"), new Course("HS2195"), new Attendance("PRESENT"),
+                new Participation("AVERAGE"), new Grade(74), List.of()),
+            new Person(new Id("U0987654U"), new Name("Tuco Salamanca"), new Phone("00000000"),
+                new Email("example@u.nus.edu"), new Course("AAA0000AA"), new Attendance("ABSENT"),
+                new Participation("UNMARKED"), new Grade(42), List.of())
         };
     }
 
